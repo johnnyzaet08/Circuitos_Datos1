7fa4b67d09264d60d48702c0ab325047b27ae94b
johnnyzaet08 <johnny.zaet08@gmail.com>
2021-01-27 16:52:31 -0600

    Bateria

---
+++ clase_resistencia.py
@@ -0,0 +1,38 @@
+
+
+class Resistencia:
+    def __init__(self,nom,valor,orientacion,boton):
+        self.nom = nom 
+        self.valor = valor
+        self.orientacion =  orientacion
+        self.boton = boton
+        self.active = False
+
+    def getNom(self):
+        return self.nom
+
+    def getValor(self):
+        return self.valor
+
+    def getOrientacion(self):
+        return self.orientacion
+    
+    def getBoton(self):
+        return self.boton
+
+    def setNom(self,nomn):
+        self.nom = nomn
+
+    def setValor(self,valorn):
+        self.valor = valorn
+
+    def setOrientacion(self,orientacionn):
+        self.orientacion = orientacionn
+
+    def setBoton(self,botonn):
+        self.boton = botonn
+
+    
+
+    
+
---
+++ ventana.py
@@ -0,0 +1,134 @@
+from tkinter import *
+from tkinter import ttk
+from threading import *
+from time import *
+from clase_resistencia import *
+
+
+
+class Ventana:
+    HOLA = None
+    ActivoResistencia = False
+    ConfiguracionNombreResitencia = None
+    ConfiguracionValorResitencia = None
+    
+    
+    
+    def __init__(self):
+        self.instancia = Tk()
+        self.B_Resistencia = None
+        self.B_fuente = None
+        self.img_resistencia = PhotoImage(file = "resitencia.png")
+        self.img_resistenciaV = PhotoImage(file = "resitenciaV.png")
+        self.img_fuente = PhotoImage(file = "fuente de poder.png")
+        self.img_Girar = PhotoImage(file = "girar.png")
+        self.img_Eliminar = PhotoImage(file = "eliminar.png")
+        self.Resistencias = []
+
+    def abrirVentana(self):
+        
+        self.instancia.title('Menu principal')
+        self.instancia.configure(bg= 'black')
+        #imagen de fondo
+        fondo = PhotoImage(file = "fondo.png")
+        fon = Label(self.instancia,image = fondo,bg = 'black')
+        fon.place(x=0,y =0)
+        #boton de resistancia
+        self.B_Resistencia = Button(self.instancia,text= 'Resistencia',image = self.img_resistencia,height = 68, width = 80,command = lambda:self.configuracion())
+        self.B_Resistencia.place(x= 80,y=450)
+        #boton de fuente
+        self.B_fuente = Button(self.instancia,text= 'fuente de poder',image = self.img_fuente,height = 68, width = 80)
+        self.B_fuente.place(x= 180,y=450)
+        self.B_Resistencia.configure()
+
+        self.instancia.bind('<Button-1>', self.motion)
+
+        self.instancia.geometry('1336x548+0+100')
+        self.instancia.mainloop()
+
+    def motion(self,event):
+        x, y = event.x, event.y       
+        ##x = self.instancia.winfo_pointerx()
+        ##y = self.instancia.winfo_pointery()
+        if self.ActivoResistencia:
+            self.ponerResistencia(x,y)
+
+    def configuracion(self):
+        self.B_Resistencia.configure(state =DISABLED)
+        self.B_fuente.configure(state =DISABLED)
+        configuracion = Canvas(self.instancia , width= 280, height = 72, bg = "black")
+        LabelNombre= Label(configuracion,text = 'Nombre',font= ('Times New Roman',15),bg= 'black',fg= 'white')
+        LabelNombre.place(x=5,y=5)
+        TextoNombre = Entry(configuracion,width = 20)
+        TextoNombre.place(x=5,y=25)
+        LabelValor= Label(configuracion,text = 'Valor',font= ('Times New Roman',15),bg= 'black',fg= 'white')
+        LabelValor.place(x=155,y=5)
+        TextoValor = Entry(configuracion,width = 20)
+        TextoValor.place(x=155,y=25)
+        btnAceptar = Button(configuracion,text= 'Aceptar',command = lambda:self.AceptarConfiguracion(configuracion,TextoNombre,TextoValor))
+        btnAceptar.place(x= 5,y=45)
+        configuracion.place(x=280, y=450)
+
+    def girarResistencia(self,resistencia,btnResistencia):
+        if resistencia.getOrientacion() == "horizontal":
+            resistencia.setOrientacion("vertical")
+            btnResistencia.configure(image = self.img_resistenciaV,width  = 68,height  = 80)
+        else:
+            resistencia.setOrientacion("horizontal")
+            btnResistencia.configure(image = self.img_resistencia,height  = 68,width   = 80)
+
+    def eliminarComponente(self,componente,canvas):
+        componente.destroy()
+        self.cierraCanvas(componente, canvas)
+
+    def cierraCanvas(self, componente, canvas):
+        componente.active = False
+        canvas.destroy()
+        
+    
+    def modifica(self,x,y,resistencia,btnResistencia):
+        if resistencia.active == False:
+            resistencia.active = True
+            canvas = Canvas(self.instancia , width= 120, height = 40, bg = "#eeaa02")
+            btnGirarImage = self.img_Girar
+            btnEliminarImage = self.img_Girar
+            btnCerrarImage = self.img_Girar
+            btnGirar = Button(canvas,image = btnGirarImage, command = lambda:self.girarResistencia(resistencia,btnResistencia))
+            btnGirar.place(x= 0,y=0)
+            btnEliminar = Button(canvas,image = btnEliminarImage, command = lambda:self.eliminarComponente(btnResistencia,canvas))
+            btnEliminar.place(x= 40,y=0)
+            btnCerrar = Button(canvas,image = btnCerrarImage, command = lambda:self.cierraCanvas(resistencia, canvas))
+            btnCerrar.place(x= 80,y=0)
+            canvas.place(x=x-10, y=y-45)
+        
+
+    def AceptarConfiguracion(self,ventana,textNom,textValor):
+        self.ConfiguracionNombreResitencia = textNom.get()
+        self.ConfiguracionValorResitencia = textValor.get()
+        ventana.destroy()
+        self.ActivoResistencia = True    
+
+    def ponerResistencia(self,x2,y2):
+        self.B_Resistencia.configure(state =NORMAL)
+        self.B_fuente.configure(state =NORMAL)
+        Resitenciab = Button(self.instancia,text= 'Resistencia',image = self.img_resistencia,height = 68, width = 80,bg = "#efe4b0", fg = "#efe4b0")
+        ResistenciaP = Resistencia(self.ConfiguracionNombreResitencia,self.ConfiguracionValorResitencia,"horizontal",Resitenciab)
+        Resitenciab.configure(command = lambda:self.modifica(x2-40,y2-34,ResistenciaP,Resitenciab))
+        Resitenciab.place(x= x2-40,y=y2-34)
+        self.Resistencias.append(ResistenciaP)
+        self.ActivoResistencia = False
+        
+    def abrir(self):
+        hilo2 = Thread(target= self.abrirVentana())
+
+   
+
+
+Ventana.HOLA = Ventana()
+Ventana.HOLA.abrir()
+
+
+    
+
+
+
